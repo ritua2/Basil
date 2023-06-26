9278ee14161b88e19daade1345d6b07c2699a100
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-06-26 10:19:17 -0400

    Update runMIDAS.sh

--- src/Python/runMIDAS.sh
+++ src/Python/runMIDAS.sh
@@ -7,18 +7,21 @@ echo -e "\nNow running MIDAS script..."
 python3 MIDAS2.py
 
 # Step 2
-echo -e "\nBuilding your custom Dockerfile..."
-docker build -t my_image -f Dockerfile .
+echo -e "\nBuilding your custom Docker Image..."
+image_id=$(docker build -t my_image -f Dockerfile . | grep -o "Successfully built [0-9a-f]*" | awk '{print $3}')
+echo -e "Image ID: $image_id"
+# grep to get id, push int to snyk and run cmd
 
 # Step 3
-echo -e "\nRunning your Docker image..."
-docker run my_image
+echo -e "\n Running SNYK analysis for your image..."
+echo -e "Image ID: $image_id"
+chmod +x snyktest.sh
+./snyktest.sh $image_id
+
 
 # Step 4
-echo -e "\nRunning SNYK deep analysis for your image..."
-chmod +x snyktest.sh
-# (make snyktest.sh accept the newly built image name)
-./snyktest.sh
+echo -e "\nRunning your Docker image..."
+docker run my_image
 
 # Step 5++ go here >>>
 # docker push (here)
