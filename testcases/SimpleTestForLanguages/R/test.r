b1e6c607b074dcae3d1f6e786777d864150315a7
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-02 19:02:32 -0400

    Create test.r

---
+++ testcases/SimpleTestForLanguages/R/test.r
@@ -0,0 +1,20 @@
+factorial <- function(n) {
+  if (n == 0) {
+    return(1)
+  } else {
+    return(n * factorial(n - 1))
+  }
+}
+
+fibonacci <- function(n) {
+  if (n == 0) {
+    return(0)
+  } else if (n == 1) {
+    return(1)
+  } else {
+    return(fibonacci(n - 1) + fibonacci(n - 2))
+  }
+}
+
+factorial(5)
+fibonacci(10)
