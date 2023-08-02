cdb96f9e569ab9e11c51a541ec7a4bfc3e629a8e
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-02 19:01:13 -0400

    Add files via upload

---
+++ testcases/SimpleTestForLanguages/Python/simple.py
@@ -0,0 +1,4 @@
+# simple.py
+# testing done by Armaan Cheema
+print("Hello Basil, I am a python script made for testing your capabilities!")
+print("---------------------------------------------------------------------")
---
+++ testcases/SimpleTestForLanguages/Python/test2.py
@@ -0,0 +1,5 @@
+# Python test case
+def add(a, b):
+    return a + b
+
+print(add(2, 3))  # Expected output: 5
