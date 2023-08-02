e295807be08a816e59f1d0a0e59020201baff02e
Sanjeeth Boddinagula <sanjeethboddi@gmail.com>
2023-08-02 15:08:52 -0500

    fix typo

--- src/Python/interactive.py
+++ src/Python/interactive.py
@@ -190,7 +190,7 @@ def main():
                 NUM+=1
         
         if len(ADVANCED_COPY) > 0:
-            midas_file.write("Advanced Copy:\n")
+            midas_file.write("Advanced copy:\n")
             for _ in range(len(ADVANCED_COPY)):
                 midas_file.write(f' {NUM}: "{ADVANCED_COPY[_][0]}:{ADVANCED_COPY[_][1]}"\n')
                 NUM+=1
