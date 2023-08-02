7986e8c1615a62df9c1a70f45ef70c824fade18a
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-02 18:57:40 -0400

    Add files via upload

---
+++ testcases/SimpleTestForLanguages/C++/test2.cpp
@@ -0,0 +1,11 @@
+// C++ test case
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    std::cout << add(2, 3) << std::endl;  // Expected output: 5
+    return 0;
+}
