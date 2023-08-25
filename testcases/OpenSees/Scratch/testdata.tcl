43914f92cb5011e2f8dcbb414f71909fc06464dd
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-25 13:54:54 -0700

    Create testdata.tcl

---
+++ testcases/OpenSees/Scratch/testdata.tcl
@@ -0,0 +1,11 @@
+# Simple OpenSees script for testing
+
+model basic -ndm 2 -ndf 3
+node 1 0.0 0.0
+node 2 1.0 0.0
+fix 1 1 1 1
+fix 2 0 1 0
+uniaxialMaterial Elastic 1 3000.0
+element zeroLength 1 1 2 -mat 1 -dir 1
+load 2 1000.0 0.0 0.0
+analyze 1
