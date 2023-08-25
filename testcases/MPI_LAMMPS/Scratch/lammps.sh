0718bead473d8fba32fbc73f825e6fac33c816b0
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-25 12:53:27 -0700

    Update lammps.sh

--- testcases/MPI_LAMMPS/Scratch/lammps.sh
+++ testcases/MPI_LAMMPS/Scratch/lammps.sh
@@ -7,11 +7,22 @@ sudo apt-get install -y openmpi-bin libopenmpi-dev git
 # Clone LAMMPS repository
 git clone https://github.com/lammps/lammps.git ~/lammps
 
-# Build LAMMPS with MPI support
+# Set the working directory
 cd ~/lammps
+
+# Build LAMMPS with MPI support
 make mpi -j$(nproc)
 
-# Display LAMMPS help
-mpirun -np 1 ~/lammps/src/lmp_mpi -h
+# Verify installation
+if [ $? -eq 0 ]; then
+    echo "LAMMPS installation successful"
+else
+    echo "LAMMPS installation failed"
+    exit 1
+fi
 
-echo "MPI LAMMPS installation and help check completed."
+# Copy your input file to the working directory
+cp /path/to/in.myinput ~/lammps/in.myinput
+
+# Run LAMMPS with your input file using 4 MPI processes
+mpirun -np 4 ~/lammps/src/lmp_mpi -in in.myinput
