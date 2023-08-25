89acd2fe70b5f946b1d739ec6ad2b929eda2a39f
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-08-25 08:15:39 -0700

    Update vinaHelp.sh

--- testcases/AutoDockVina/Scratch/vinaHelp.sh
+++ testcases/AutoDockVina/Scratch/vinaHelp.sh
@@ -6,11 +6,11 @@ tar -zxvf autodock_vina_1_1_2_linux_x86.tgz
 
 cd autodock_vina_1_1_2_linux_x86/vina
 
-#Prepare Ligand and Receptor Files if needed
+# prepare Ligand and Receptor files if needed
 # obabel ligand.pdb -O ligand.pdbqt
 # obabel receptor.pdb -O receptor.pdbqt
 
-./vina -help
+./vina --help
 
 rm ligand.pdbqt receptor.pdbqt
 cd ..
