17a7af4ae5a8b827eaaf4ae2925e25b627b2ae3b
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-10-04 09:43:05 -0400

    Update parser.py
    ongoing singularity work

--- src/Python/parser.py
+++ src/Python/parser.py
@@ -7,15 +7,11 @@ Parses an input file written in JSON or YAML for now, and is to be extended to p
 import os
 import yaml
 
-
-
 # Returns a dictionary containing the data
 def parse_commands(filepath):
 
     with open(filepath, "r") as ff:
         return yaml.safe_load(ff)
-
-
 
 # Finds a list of all provided commands
 # Requires the data in dictionary form
@@ -24,8 +20,6 @@ def provided_instructions(raw_data):
     valid_tags = ["Base", "Working directory", "Setup", "Contents", "Environment variables", "Default command", "Entry command", "Expose ports", "Volumes", "Advanced copy"]
 
     return [key for key in raw_data if key in valid_tags]
-
-
 
 # Checks that, a minimum, the base exists
 # If so, it returns [Dockerfile syntax, False]
@@ -37,11 +31,15 @@ def base_check(raw_data):
 
     return [raw_data["Base"], False]
 
+def image_check(raw_data):
 
+    if "Image type" not in raw_data:
+        return ["INVALID: Image type was not selected", True]
+
+    return [raw_data["Image type"], False]
 
 # Orders all inputs according to their order
 def order_inputs(raw_data):
-
 
     tags_to_order = provided_instructions(raw_data)
 
@@ -58,7 +56,6 @@ def order_inputs(raw_data):
         for numbered_order, instruction in raw_data[atype].items():
             all_tags.append([int(numbered_order), instruction, atype])
 
-
     # Sorts them by location
     all_tags.sort(key=lambda x: x[0])
 
@@ -69,8 +66,6 @@ def order_inputs(raw_data):
         return ["INVALID: There cannot be two instructions with the same order priority", True]
     else:
         return [all_tags, False]
-
-
 
 # From the list of all tags, it combines commands which are next to each other
 # already_sorted_instructions (arr) [order (int), instruction (str), type (str)]
@@ -103,58 +98,46 @@ def commands_combiner(already_sorted_instructions, spacing="    "):
 
         an_instruction[1] = successive_commands
 
-
     return already_sorted_instructions
-
 
 ##########################
 # Processes a series of instructions
 ##########################
 
-
 # base image
 def df_base(basename):
-    return "FROM "+basename
-
+    return "From "+basename
 
 # Working directory
 def df_workdir(workdir):
     return "WORKDIR "+workdir
 
-
 # Run commands
 # run_comm (arr) (str): Contains several commands to provision the container
 def df_run(run_comm, spacing="    "):
-    return "RUN "+(" &&\\\n"+spacing).join(run_comm)
-
+    return "%post\n    "+(" &&\\\n"+spacing).join(run_comm)
 
 # Copies a file
 # If the path is not specified, it is assumed to be in the current working directory of the container
-def df_copy(copy_instruction):
-
-    copy_broken = copy_instruction.split(":")
-    if len(copy_broken) == 1:
-        return "COPY "+copy_broken[0]+" ."
-    else:
-        return "COPY "+copy_broken[0]+" "+copy_broken[1]
-
+def df_copy(copy_instruction, spacing="    "):
+    return "%files\n"+spacing+(copy_instruction)
 
 # Adds an environmental variable
 # If the path is not specified, it is assumed to be in the current working directory of the container
 def df_env(envar_instruction):
     env_broken = envar_instruction.split(":")
-    return "ENV "+env_broken[0]+" "+env_broken[1]
-
-
-
+    if len(env_broken) != 2:
+        raise ValueError("Invalid environment instruction format")
+    return "%environment\nexport " + env_broken[0].strip() + "=" + env_broken[1].strip()
 
 # Prints the default command
 def df_cmd(cmd_instruction):
-
+    """Convert Docker CMD instructions to Singularity %runscript."""
+    
     cmd_space_broken = cmd_instruction.split(" ")
-    cmd_space_broken = ["\""+a+"\"" for a in cmd_space_broken if a != '']
+    cleaned_cmds = [cmd for cmd in cmd_space_broken if cmd != '']
 
-    return "CMD [" + ", ".join(cmd_space_broken) + "]"
+    return "%runscript\n    " + " ".join(cleaned_cmds)
 
 def df_ent(ent_instruction):
         ent_space_broken = ent_instruction.split(" ")
@@ -174,9 +157,6 @@ def df_acopy(acopy_instruction):
         return "ADD "+copy_broken[0]+" "+copy_broken[1]
     return ""
     
-
-
-
 # Given all instructions, it processes them and writes them into a file
 # dockerfile_path (str): path of the created Dockerfile
 # image_base (str): image base
@@ -204,20 +184,23 @@ def write_to_dockerfile(dockerfile_path, image_base, ordered_instructions, defau
             elif an_instruction[2] == "Advanced copy":
                 dfp.write(df_acopy(an_instruction[1])+"\n")
 
-            
-
         if entry_comm:
                 dfp.write("\n"+df_ent(entry_comm)+"\n")
         if default_comm:
             dfp.write("\n"+df_cmd(default_comm)+"\n")
         
 
-
-
 # Given a json/yaml file with the instructions, it creates the dockerfile
 # file_with_data (str): path to the file to be parsed
-def create_dockerfile(file_with_data, dockerfile_path, spacing="    "):
+def create_imagefile(file_with_data, dockerfile_path, spacing="    "):
     original_data = parse_commands(file_with_data)
+
+    it = image_check(original_data)
+    if it[1]:
+        return it[0]
+    
+    image_type_check = it[0]
+    print(image_type_check)
 
     bc = base_check(original_data)
 
@@ -238,4 +221,3 @@ def create_dockerfile(file_with_data, dockerfile_path, spacing="    "):
     
     write_to_dockerfile(dockerfile_path, image_base, combined_inputs, original_data.get("Default command",False), original_data.get("Entry command",False), spacing)
     return "Dockerfile created at '"+dockerfile_path+"'"
-
